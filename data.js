451032908dfe686853b134689074f2047535cff1
Matthew <matthew-03@mail.ru>
2024-04-09 20:03:35 +0300

    yea

---
+++ data.js
@@ -0,0 +1,58 @@
+const data = `
+    [
+        {
+            "src": "img/product1.jpg",
+            "alt": "img obj data",
+            "name": "ELLERY X MO CAPSULE",
+            "link": "product.html",
+            "desc": "Known for her sculptural takes on traditional tailoring, Australian",
+            "price": "$52.00",
+            "cart": "img/Add to cart.svg"
+        },
+        {
+            "src": "img/product6.jpg",
+            "alt": "img obj data",
+            "name": "ELLERY X MO CAPSULE",
+            "link": "product.html",
+            "desc": "Known for her sculptural takes on traditional tailoring, Australian",
+            "price": "$52.00",
+            "cart": "img/Add to cart.svg"
+        },
+        {
+            "src": "img/product3.jpg",
+            "alt": "img obj data",
+            "name": "ELLERY X MO CAPSULE",
+            "link": "product.html",
+            "desc": "Known for her sculptural takes on traditional tailoring, Australian",
+            "price": "$52.00",
+            "cart": "img/Add to cart.svg"
+        },
+        {
+            "src": "img/product4.jpg",
+            "alt": "img obj data",
+            "name": "ELLERY X MO CAPSULE",
+            "link": "product.html",
+            "desc": "Known for her sculptural takes on traditional tailoring, Australian",
+            "price": "$52.00",
+            "cart": "img/Add to cart.svg"
+        },
+        {
+            "src": "img/product5.jpg",
+            "alt": "img obj data",
+            "name": "ELLERY X MO CAPSULE",
+            "link": "product.html",
+            "desc": "Known for her sculptural takes on traditional tailoring, Australian",
+            "price": "$52.00",
+            "cart": "img/Add to cart.svg"
+        },
+        {
+            "src": "img/product6.jpg",
+            "alt": "img obj data",
+            "name": "ELLERY X MO CAPSULE",
+            "link": "product.html",
+            "desc": "Known for her sculptural takes on traditional tailoring, Australian",
+            "price": "$52.00",
+            "cart": "img/Add to cart.svg"
+        }
+    ] 
+`
